c715a62576aee7aa599408825d2f94bcf0cb2ac5
Abhinav Singh <developwithabhinav@gmail.com>
2024-12-01 21:37:45 -0500

    Edit thing working

--- routes/news.js
+++ routes/news.js
@@ -24,13 +24,12 @@ router.post('/add', ensureAuthenticated, async (req, res) => {
 // Protected - Edit news
 router.get('/edit/:id', ensureAuthenticated, async (req, res) => {
   const newsItem = await News.findById(req.params.id);
-  const { title, content, author, weblink } = newsItem;
   res.render('news/edit', { news: newsItem });
 });
 
 router.post('/edit/:id', ensureAuthenticated, async (req, res) => {
   let { title, content, author, weblink } = req.body;
-  // content = content.slice(0, 128);
+  content = content.slice(0, 128);
   await News.findByIdAndUpdate(req.params.id, { title, content, author, weblink });
   res.redirect('/news');
 });
